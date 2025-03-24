b4d10cfd04acea6a769585b7ebc4a3585cc29297
nicolepaulina027 <nicolepaulina027@gmail.com>
2025-03-24 11:36:15 -0400

    Add files via upload

---
+++ Funciones.js
@@ -0,0 +1,13 @@
+
+function saludar(nombre) {
+    return `Hola, ${nombre}!`;
+}
+console.log(saludar("Carlos"));
+
+const sumar = function(a, b) {
+    return a + b;
+};
+console.log(sumar(5, 3));
+
+const multiplicar = (x, y) => x * y;
+console.log(multiplicar(4, 2));
